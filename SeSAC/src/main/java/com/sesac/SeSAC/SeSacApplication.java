a218d208447b8e8ce1e90d4b2403aa32da4a3ffd
ljh <dusk5588@naver.com>
2023-06-01 13:57:35 +0900

    1

---
+++ SeSAC/src/main/java/com/sesac/SeSAC/SeSacApplication.java
@@ -0,0 +1,13 @@
+package com.sesac.SeSAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeSacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeSacApplication.class, args);
+	}
+
+}
---
+++ SeSAC/src/test/java/com/sesac/SeSAC/SeSacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesac.SeSAC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeSacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
