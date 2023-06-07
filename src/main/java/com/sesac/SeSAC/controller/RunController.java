4ed98d7bc780bf1ca511eabddeabd7184a8f1c07
musung <musungkim1352@gmail.com>
2023-06-07 11:51:38 +0900

    mod controller

--- src/main/java/com/sesac/SeSAC/controller/RunController.java
+++ src/main/java/com/sesac/SeSAC/controller/RunController.java
@@ -1,19 +1,15 @@
 package com.sesac.SeSAC.controller;
 
-import com.sesac.SeSAC.dto.RunningForm;
+import com.sesac.SeSAC.model.RunningInfo;
 import com.sesac.SeSAC.service.DangerService;
 import com.sesac.SeSAC.service.RunningService;
+import com.sesac.SeSAC.util.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,18 +17,19 @@ import java.nio.file.StandardCopyOption;
 public class RunController {
     final RunningService runningService;
     final DangerService dangerService;
+    final ImageService imageService;
     private String uploadPath = "/Users/musung/testPath";
-    @PostMapping("/runningInfo")
-    public ResponseEntity<String> postRunningInfo(@RequestBody MultipartFile file, @RequestBody RunningForm runningForm){
-        try {
-            String fileName = file.getOriginalFilename();
-            Path filePath = Path.of(uploadPath + File.separator + fileName);
-            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-            System.out.println(runningForm.getFirst());
-            return ResponseEntity.ok("이미지가 성공적으로 저장되었습니다.");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("이미지 저장 중 오류가 발생했습니다.");
+    @PostMapping("/saveImage")
+    public String saveImage(@RequestBody MultipartFile file) throws IOException {
+        try{
+            return imageService.saveImage(uploadPath,file);
         }
+        catch(IOException e){
+            return "fail";
+        }
+    }
+    @PostMapping("/runingInfo")
+    public void postRunningInfo(@RequestBody RunningInfo runningInfo){
+        runningService.join(runningInfo);
     }
 }
